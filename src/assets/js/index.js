b7fd6a642cb83dd678019185ae12f56082f0dbdb
5572189 <1542510269@qq.com>
2017-03-14 14:40:18 +0800

    incake-wap-2017

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -4,44 +4,98 @@
 	$(function() {
 		//加载图片
 		initData();
-		
+		//节流加载
 		var throttle = _.throttle(scroll, 200);
 		$(window).on('scroll', throttle);
+
+		//swiper图片轮播
+		var swiper = new Swiper('.swiper-container', {
+			pagination: '.swiper-pagination',
+			paginationClickable: true,
+			spaceBetween: 30,
+			centeredSlides: true,
+			autoplay: 2500,
+			autoplayDisableOnInteraction: false
+		});
+
+		//筛选
+		$(".screen").click(function() {
+			$("#shade").fadeIn(function() {
+				$(".list").animate({ right: '0px' });
+			});
+			$("body").on('touchmove.mask', function(event) {
+				event.preventDefault();
+				event.stopPropagation();
+			});
+		});
+
+		//点击筛选，出现遮罩
+		$("#shade").click(function() {
+			$(".list").animate({ right: '-550px' }, function() {
+				$("#shade").fadeOut();
+			});
+			$("body").off(".mask");
+		});
+
+		//筛选全部点击
+		$(".all").click(function() {
+			$(this).addClass('border');
+			$(this).siblings().removeClass('active').css("text-align", "center");
+		});
+
+		//筛选列表全部点击
+		$(".own").click(function() {
+			$(".all").addClass("border");
+			$(".love").removeClass("active").css("text-align", "center");
+		});
+
+		//筛选点击绑定
+		$("#idscreen").on('click', '.love', function() {
+			var flag = $(".love").hasClass("active") ? true : false;
+			if(flag) {
+				$(this).remove();
+				$(".all").addClass("border");
+			} else {
+				$(".love").addClass("active");
+			}
+			$(this).addClass("active").css("text-align", "left");
+			$(this).siblings().removeClass("border");
+		});
+
+		//筛选列表点击
+		$(".kind").click(function() {
+			$(".love").html($(this).html()).addClass("active").css("text-align", "left");;
+			$(".all").removeClass("border")
+			var num = $("#idscreen").find('a');
+			if(num.length == 1) {
+				$("<a href='javascript:;'class='love active'/></a>").appendTo("#idscreen");
+				$(".love").html($(this).html());
+				$(".all").removeClass("border");
+			}
+		});
+
+		//蛋糕收藏
+		$("#main").on("click", ".collect", function() {
+			if($(this).hasClass("selected")) {
+				$(this).removeClass("selected");
+			} else {
+				$(this).addClass("selected");
+			}
+		});
 	});
-	
+
 	//scroll事件
 	function scroll() {
 		var scrollTop = document.documentElement.scrollTop || document.body.scrollTop,
 			viewHeight = $(window).height(),
 			mainTop = $("#main").offset().top,
-			mainHeight = $("#main").height(),
-			mainpaddingBottom = parseInt($("#main").css("padding-bottom"));
-
-		var disT = (scrollTop + viewHeight) - (mainTop + mainHeight) - mainpaddingBottom + 10;
+			mainHeight = $("#main").height() + $("#loading").height();
+		//		var	mainpaddingBottom = parseInt($("#loading").css("padding-bottom"));
+		var disT = (scrollTop + viewHeight) - (mainTop + mainHeight);
 		if(disT > 0) {
 			initData();
 		}
-
 	}
-	//swiper图片轮播
-	var swiper = new Swiper('.swiper-container', {
-		pagination: '.swiper-pagination',
-		spaceBetween: 30,
-		autoplay: 2500,
-	});
-	//筛选
-	$(".screen").click(function() {
-		$("#shade").fadeIn(function() {
-			$(".list").animate({ right: '0px' });
-		});
-		$('html,body').addClass('ovfHiden');
-	})
-	$("#shade").click(function() {
-		$(".list").animate({ right: '-550px' }, function() {
-			$("#shade").fadeOut();
-		});
-		$('html,body').removeClass('ovfHiden');
-	});
 
 	function initData() {
 		// ajax
