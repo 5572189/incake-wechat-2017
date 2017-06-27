df83ba2cf712d5c66cdd40d98b9a11d6bacafa81
5572189 <1542510269@qq.com>
2017-06-27 17:24:03 +0800

    shopping-car图片裁剪

--- src/assets/js/shopping-cart.js
+++ src/assets/js/shopping-cart.js
@@ -1,6 +1,7 @@
 ;(function($, window, document) {
-
+	var tl = new TimelineLite();
     $(function(){
+
 		var swiper01 = new Swiper('.swiper-container01', {
 	        pagination: '.swiper-pagination',
 	        slidesPerView: 'auto',
@@ -20,13 +21,13 @@
     	Cake();
     });
     function Cake(){
-    	
+
     	var $number_swiper=$("#number-swiper");
-    	var $tableware=$("#tableware");
+    	// var $tableware=$("#tableware");
     	var data = {
 	    	list :[{
 	    		aimg:"assets/imgs/shopping-cart/soldout_bg.png",
-	    		img:"assets/imgs/shopping-cart/cake.png",
+	    		img:"assets/imgs/shopping-cart/candle01.png",
 	    		name:'芒果拿破仑',
 	    		nameEn:"Mango Napoleon",
 	    		cost:'189',
@@ -34,7 +35,7 @@
 	    		linkimg:"assets/imgs/shopping-cart/shopping.png"
 	    	},{
 	    		aimg:"assets/imgs/shopping-cart/soldout_bg.png",
-	    		img:"assets/imgs/shopping-cart/cake.png",
+	    		img:"assets/imgs/shopping-cart/candle02.png",
 	    		name:'芒果拿破仑',
 	    		nameEn:"Mango Napoleon",
 	    		cost:'189',
@@ -42,23 +43,7 @@
 	    		linkimg:"assets/imgs/shopping-cart/shopping.png"
 	    	},{
 	    		aimg:"assets/imgs/shopping-cart/soldout_bg.png",
-	    		img:"assets/imgs/shopping-cart/cake.png",
-	    		name:'芒果拿破仑',
-	    		nameEn:"Mango Napoleon",
-	    		cost:'189',
-	    		price:'1.5',
-	    		linkimg:"assets/imgs/shopping-cart/shopping.png"
-	    	},{
-	    		aimg:"assets/imgs/shopping-cart/soldout_bg.png",
-	    		img:"assets/imgs/shopping-cart/cake.png",
-	    		name:'芒果拿破仑',
-	    		nameEn:"Mango Napoleon",
-	    		cost:'189',
-	    		price:'1.5',
-	    		linkimg:"assets/imgs/shopping-cart/shopping.png"
-	    	},{
-	    		aimg:"assets/imgs/shopping-cart/soldout_bg.png",
-	    		img:"assets/imgs/shopping-cart/cake.png",
+	    		img:"assets/imgs/shopping-cart/candle03.jpg",
 	    		name:'芒果拿破仑',
 	    		nameEn:"Mango Napoleon",
 	    		cost:'189',
@@ -67,58 +52,82 @@
 	    	}]
 	    };
 	    var tplnumber = template('tplnumber', data);
-	    var tplTableware = template('tplTableware', data);
+	    // var tplTableware = template('tplTableware', data);
 	    $number_swiper.html(tplnumber);
-	    $tableware.html(tplTableware);
+	    // $tableware.html(tplTableware);
     };
-    
-    
+
+
 	$('.txt-summary').maxlength({
         max: 100,
         feedbackText: '还可输入{r}字'
     });
-	var $cake_list = $("#cake_list ul li"),
+	var $cake = $("#cake_list"),
+		$cake_list = $cake.find('ul li'),
 		$pitch_on = $cake_list.find(".pitch-on"),
+		$icons = $cake_list.find(".icons"),
+		$selectA = $cake_list.find("a"),
 		$other = $(".other ul li"),
 		$commodity = $(".commodity li>span"),
-		$privilege = $(".privilege ul li>span");
-	$(".swiper-container02").css("display","none");	
+		$privilege = $(".privilege ul li>span"),
+		$all_select = $('.all-select');
+    //添加全选功能
+	$all_select.on('click',function(){
+		var isActive = $(this).hasClass('active');
+		var $haveActive = $cake_list.find("a.active");
+		if(!isActive){
+			$(this).addClass('active');
+			if($haveActive){
+				$haveActive.prev('.icons').removeClass('active');
+				$icons.not($haveActive.prev('.icons')).addClass('active');
+			}
+		}else{
+			$(this).removeClass('active');
+			$icons.removeClass('active');
+		}
+	});
+
+	$(".swiper-container02").css("display","none");
 	$other.click(function(){
 		var $this = $(this);
         itemIndex = $this.index();
 		$this.addClass("active").siblings().removeClass("active");
 		 $('.lists').eq(itemIndex).show().siblings('.lists').hide();
 	});
-	
+
 	$privilege.click(function(){
-		if(!$(this).hasClass("active")){
+
+		if(!$(this).hasClass("active") && !$(this).next('a').hasClass('active')){
 			$(this).addClass("active");
 		}else{
 			$(this).removeClass("active");
+			$('.all-select').removeClass('active');
 		}
 	});
 	$commodity.click(function(){
-		if(!$(this).hasClass("active")){
+		if(!$(this).hasClass("active") && !$(this).next('a').hasClass('active')){
 			$(this).addClass("active");
 		}else{
+			$('.all-select').removeClass('active');
 			$(this).removeClass("active");
 		}
 	})
 	$pitch_on.click(function(){
-		if(!$(this).hasClass("active")){
+		if(!$(this).hasClass("active") && !$(this).next('a').hasClass('active')){
 			$(this).addClass("active");
 		}else{
 			$(this).removeClass("active");
+			$('.all-select').removeClass('active');
 		}
 	});
-	
+
 	function add(add,minus,number){
 		var $add = $(add),
 			$subtract = $(minus),
 			num = 0;
-		
+
 		$add.click(function(){
-			
+
 			amout = parseInt($(number).html());
 			amout++;
 			if(amout > 1){
@@ -131,6 +140,10 @@
 				return false;
 			}
 			amout = parseInt($(number).html());
+			if(amout === 1){
+				return false;
+			}
+
 			amout--;
 			if(amout <= 1){
 				$(this).addClass('disabled');
@@ -160,12 +173,12 @@
 			$shopping_here = $(".shopping-here"),
 			$delete = $(".delete"),
 			flage = true;
-		
-		
+
+
 		$delete.click(function(){
 			$(this).closest('li').remove();
 		})
-		
+
 		$surprised_here.click(function() {
 			$this = $(this);
 			$shade.fadeIn(function(){
@@ -191,21 +204,21 @@
 				});
 			}
 		});
-		
+
 
 		$shopping_here.click(function(){
 			$shopping_here.fadeOut(function(){
 				$shopping_cart.fadeOut();
 			})
 		})
-		
+
 		$shade_li.click(function(){
 			$(this).addClass("active").siblings().removeClass("active");
 		});
 		$shade_other01.click(function(){
 			$last_li01.focus();
 		});
-		
+
 		$shade_other02.click(function(){
 			$last_li02.focus();
 		});
@@ -218,24 +231,25 @@
 	})();
 
 
-	
-	    
+
+    //惊喜图片裁剪
 	(function(){
 		var $image = $('#image01'),
   			$file = $("#file01"),
  			$page01 = $("body"),
  			$imagesrc01 = $(".imagesrc01");
-	 			
+
 	 	$image.cropper({
 	 		aspectRatio: 1 / 1,
-        	autoCropArea: 1,
-			movable: false,
-	        zoomable: false,
-	        rotatable: false,
-	        scalable: false,
-	        background:false,
-	        modal:false,
-        	minContainerHeight:500
+	      autoCropArea: 0.8,
+	      movable: true,
+	      zoomable: true,
+	      rotatable: true,
+	      scalable: true,
+	      background: true,
+	      modal: true,
+	      minContainerHeight: 200,
+	      zoomOnTouch: true
 		});
 		$page01.on('click', '.inputFile01', function(e) {
 	        $inputImage = $(this);
@@ -249,14 +263,14 @@
 	                if (!$image.data('cropper')) {
 	                    return;
 	                }
-	
+
 	                if (files && files.length) {
 	                    file = files[0];
-	
+
 	                    if (/^image\/\w+$/.test(file.type)) {
 	                        blobURL = URL.createObjectURL(file);
 	                        $image.one('built.cropper', function() {
-	
+
 	                            // Revoke when load complete
 	                            URL.revokeObjectURL(blobURL);
 	                        }).cropper('reset').cropper('replace', blobURL);
@@ -270,7 +284,7 @@
 	            });
 	        }
 	    });
-	    
+
 	  	$(".tailor01").click(function(){
 	  		var img = $image.cropper('getCroppedCanvas', {
 	        width: 600,
@@ -279,12 +293,11 @@
 	    	$(".container01").fadeOut();
 	    	$imagesrc01.attr("src",img);
 	  	});
-	  	$(".reupload-image01").click(function(){
-	  		$(".containe01").fadeOut();
-	  	});
-	  
-	})();  
-//	裁剪
+	 	$(".reupload-image01").click(function(){
+  			$(".container01").fadeOut();
+	 	});
+	})();
+    //上传裁剪
 	(function(){
 		var $image = $('#image'),
   			$file = $("#file"),
@@ -295,17 +308,18 @@
  			$picture=$(".picture"),
  			$picture_close=$(".picture-close"),
  			pid="0";
-	 			
+
 	 	$image.cropper({
 	 		aspectRatio: 1 / 1,
-        	autoCropArea: 1,
-			movable: false,
-	        zoomable: false,
-	        rotatable: false,
-	        scalable: false,
-	        background:false,
-	        modal:false,
-        	minContainerHeight:500
+		      autoCropArea: 0.8,
+		      movable: true,
+		      zoomable: true,
+		      rotatable: true,
+		      scalable: true,
+		      background: true,
+		      modal: true,
+		      minContainerHeight: 200,
+		      zoomOnTouch: true
 		});
 		$page.on('click', '.inputFile', function(e) {
 	        $inputImage = $(this);
@@ -320,14 +334,14 @@
 	                if (!$image.data('cropper')) {
 	                    return;
 	                }
-	
+
 	                if (files && files.length) {
 	                    file = files[0];
-	
+
 	                    if (/^image\/\w+$/.test(file.type)) {
 	                        blobURL = URL.createObjectURL(file);
 	                        $image.one('built.cropper', function() {
-	
+
 	                            // Revoke when load complete
 	                            URL.revokeObjectURL(blobURL);
 	                        }).cropper('reset').cropper('replace', blobURL);
@@ -341,21 +355,19 @@
 	            });
 	        }
 	    });
-	    
-	  	$(".tailor").click(function(){
+
+	  	$(".container").on('click','.tailor',function(){
 	  		var img = $image.cropper('getCroppedCanvas', {
 	        width: 600,
 	        height: 600
 	    	}).toDataURL('image/jpeg');
 	    	$imagesrc.attr("src",img);
 	    	$(".container").fadeOut();
-	    var currentli=$('.convention').children('li[pid='+pid+']');
-	    currentli.attr("viewImage",img);
-	    currentli.find('.uploading').text('预览');
+    	    var currentli=$('.convention').children('li[pid='+pid+']');
+    	    currentli.attr("viewImage",img);
+    	    currentli.find('.uploading').text('预览');
 	  	});
-	  	$(".reupload-image").click(function(){
-	  		$(".container").fadeOut();
-	  	});
+
 	  	$(".uploading").click(function(){
 	  		var $this=$(this);
 	  		if($this.text()==='预览'){
@@ -367,11 +379,144 @@
 	  			});
 	  		}
 		});
-		  	
+        $(".reupload-image").click(function(){
+	  		$(".container").fadeOut();
+	  	});
 	 	$picture_close.click(function(){
   			$picture_shade.fadeOut();
   		});
-	  
+
 	})();
-	
+
+	//蜡烛加减
+	;(function(){
+		var amout = 0;
+		$(".candle").on("click",".addNumber",function(event){
+			event.preventDefault();
+			event.stopPropagation();
+			var $this = $(this);
+			amout = parseInt($this.siblings('.number').text());
+			amout++;
+
+			if(amout > 0){
+				$this.siblings('.subtract').removeClass('disabled').addClass('active');
+			}
+			$this.siblings('.number').text(amout);
+		});
+		$(".candle").on("click",".subtract",function(event){
+			event.preventDefault();
+			event.stopPropagation();
+			var $this = $(this);
+
+			if($this.hasClass("disabled")) {
+				return false;
+			}
+			amout = parseInt($this.siblings('.number').text());
+			if(amout === 0) {
+				$this.addClass('disabled').removeClass('active');
+				return false;
+			}
+			amout--;
+			if(amout <= 0) {
+				$this.addClass('disabled').removeClass('active');
+			}
+			$this.siblings('.number').text(amout);
+		});
+	})();
+	//盘叉加减
+	;(function(){
+		var amout = 0;
+		$(".dish").on("click",".addNumber",function(event){
+			event.preventDefault();
+			event.stopPropagation();
+			var $this = $(this);
+			amout = parseInt($this.siblings('.number').text());
+			amout++;
+
+			if(amout > 0){
+				$this.siblings('.subtract').removeClass('disabled').addClass('active');
+			}
+			$this.siblings('.number').text(amout);
+		});
+		$(".dish").on("click",".subtract",function(event){
+			event.preventDefault();
+			event.stopPropagation();
+			var $this = $(this);
+
+			if($this.hasClass("disabled")) {
+				return false;
+			}
+			amout = parseInt($this.siblings('.number').text());
+			if(amout === 0) {
+				$this.addClass('disabled').removeClass('active');
+				return false;
+			}
+			amout--;
+			if(amout <= 0) {
+				$this.addClass('disabled').removeClass('active');
+			}
+			$this.siblings('.number').text(amout);
+		});
+	})();
+	$("#number-swiper").on('click','.select-parcel',function(){
+		tl.clear();
+		tl.to($(".parcel"), 0.5, {
+			bottom: "0%",
+			onStart: function() {
+				$(".parcel").css("display","block");
+				$(".shade").fadeIn();
+			}
+		});
+	});
+	$("#tableware").on('click','.select-dish',function(){
+		tl.clear();
+		tl.to($(".dish"), 0.5, {
+			bottom: "0%",
+			onStart: function() {
+				$(".dish").css("display","block");
+				$(".shade").fadeIn();
+			}
+		});
+	});
+	//取消
+	$(".cancel").click(function(){
+		tl.clear();
+		tl.to($('.parcel'),0.5,{
+			bottom: "-100%",
+			onComplete:function(){
+				$(".shade").fadeOut();
+			}
+		});
+		tl.to($('.dish'),0.5,{
+			bottom: "-100%"
+		});
+	});
+	//确定
+	$(".confirm-select").click(function(){
+		tl.clear();
+		tl.to($('.parcel'),0.5,{
+			bottom: "-100%",
+			onComplete:function(){
+				$(".shade").fadeOut();
+			}
+		});
+		tl.to($('.dish'),0.5,{
+			bottom: "-100%"
+		});
+	});
+	//遮罩空白
+	$(".shade").click(function(e){
+		if(e.target===$(".shade")[0]){
+			tl.clear();
+			tl.to($('.parcel'),0.5,{
+				bottom: "-100%",
+				onComplete:function(){
+					$(".shade").fadeOut();
+				}
+			});
+			tl.to($('.dish'),0.5,{
+				bottom: "-100%"
+			});
+		}
+	});
 })(jQuery, window, document);
