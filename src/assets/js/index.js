389f02437592ed573106d79691501945cf35cf1a
5572189 <1542510269@qq.com>
2017-07-14 17:01:37 +0800

    miandian

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -32,6 +32,10 @@
     });
 
     $(function() {
+
+        // 瑞雪检测 --- 首页
+        fnInitRxHome();
+
         var tl = new TimelineLite();
         //加载图片
         initData();
@@ -172,12 +176,14 @@
         });
 
         //蛋糕收藏
-        $("#main").on("click", ".collect", function() {
+        $("#main").on("click", ".collect", function(e) {
             if ($(this).hasClass("selected")) {
                 $(this).removeClass("selected");
             } else {
                 $(this).addClass("selected");
             }
+            e.preventDefault();
+            e.stopPropagation();
         });
 
         //城市定位
@@ -239,6 +245,9 @@
 
         //数据绑定
         $("#main").on('click', '.icon-add', function() {
+
+            var change = $(this).closest('.box-text').find('.change').html().trim();
+
 
             var strSc = $(this).closest('.box').attr('data-sc').trim(),
                 strRegular = $(this).closest('.box').attr('data-regular').trim(),
@@ -309,15 +318,17 @@
                 $(".prices").text(parseInt(prices, 10));
             });
 
-            handle4BindShade(data);
+            handle4BindShade(data,change);
         });
 
         // 绑定加入购物车弹框数据
-        function handle4BindShade(data) {
+        function handle4BindShade(data,change) {
             var _html = template('tplNormsShade', data);
             $('#normsShade').html(_html);
 
-            $(".norms-shade").fadeIn();
+            $(".norms-shade").fadeIn(function(){
+                $('.hid_htmlname').val(change);
+            });
         }
 
         var $add_surprised = $(".add-surprised"),
@@ -644,4 +655,239 @@
         $boxRight.append(_html);
     }
 
+    function fnInitRxHome(){
+        if(!rxStream){
+            return false;
+        }
+        var $hid_username = $('#hid_username');
+
+        var o_username = '',
+            o_mobile = '',
+            b_device = 'wap';
+
+        if($hid_username.length > 0){
+            o_username = $hid_username.val();
+            o_mobile = $hid_username.val();
+        }
+        //访问首页
+        (function(){
+            rxStream.track('visit_homepage',{
+                subject: {
+                    o_username : o_username,
+                    o_mobile : o_mobile
+                },
+                properties : {
+                    b_device : b_device
+                }
+            });
+        })();
+
+        //banner 广告
+        $('.swiper-container').on('click','.swiper-slide',function(e){
+            var slidelen = $(this).closest('.swiper-wrapper').find('.swiper-slide:not(.swiper-slide-duplicate)').length;
+                b_ad_number = 0,
+                b_ad_type = '';
+
+            b_ad_number = $(this).index();
+            if(b_ad_number > slidelen){
+                b_ad_number = b_ad_number - slidelen;
+            }
+
+            b_ad_title = $(this).attr('data-adtitle').trim();
+            b_ad_type = $(this).attr('data-adtype').trim();
+            // console.log(b_ad_title)
+            // console.log(b_ad_type)
+            // send to rxstream server
+            rxStream.track('ad_banner',{
+                subject: {
+                    o_username : o_username,
+                    o_mobile : o_mobile
+                },
+                properties: {
+                    b_ad_title: b_ad_title,
+                    b_ad_number: b_ad_number,
+                    b_ad_type: b_ad_type,
+                    b_device: b_device
+                }
+            });
+        });
+
+        // 首页分类入口
+        $('#idxSection').on('click','a',function(){
+            var b_menu = $(this).attr('data-name').trim();
+            //console.log(b_menu)
+            // send to rxstream server
+			rxStream.track('ad_category', {
+				subject: {
+					o_username: o_username,
+					o_mobile: o_mobile
+				},
+				properties: {
+					b_menu: b_menu,
+					b_device: b_device
+				}
+			});
+        });
+
+        //加入购物车
+        $('#normsShade').on('click','.buy',function(e){
+            var b_productname = '',
+                b_product_size = '',
+                b_productprice_d = '',
+                b_productprice_m = 0,
+                b_productCount_d = 0,
+                b_productstyle = '';
+
+            var $norms = $(this).closest('.norms'),
+                $item = $norms.find('.headline'),
+                $wrapper = $norms.find('.parcel'),
+                currType = $item.find('li').filter('.active').index(),
+                $specbox = $wrapper.find('.spec-box').eq(currType),
+                $pounditem = $specbox.find('.item-rule li').filter('.active');
+
+            b_productname = $norms.find('.hid_htmlname').val();
+            b_product_size = $pounditem.html().trim();
+            b_productprice_d = $pounditem.attr('data-prices');
+            b_productprice_m = parseFloat(b_productprice_d, 10).toFixed(2);
+            b_productCount_d = $specbox.find('.number_en').html();
+            b_productstyle = $item.find('li').filter('.active').html().trim();
+            // console.log(b_productname)
+            // console.log(b_product_size)
+            // console.log(b_productprice_d)
+            // console.log(b_productprice_m)
+            // console.log(b_productCount_d)
+            // console.log(b_productstyle)
+            // send to rxstream server
+			rxStream.track('add_shoppingcart', {
+				subject: {
+					o_username: o_username,
+					o_mobile: o_mobile
+				},
+				properties: {
+                      b_productname: b_productname,
+                      b_product_size: b_product_size,
+                      b_productprice_d: b_productprice_d,
+                      b_productprice_m: b_productprice_m,
+                      b_productCount_d: b_productCount_d,
+                      b_productstyle: b_productstyle,
+					  b_device: b_device
+				}
+			});
+
+            e.stopPropagation();
+
+        });
+
+        //立即购买
+        $('#normsShade').on('click','.add-shop',function(e){
+            var b_productname = '',
+                b_product_size = '',
+                b_productprice_d = '',
+                b_productprice_m = 0,
+                b_productCount_d = 0,
+                b_productstyle = '';
+
+            var $norms = $(this).closest('.norms'),
+                $item = $norms.find('.headline'),
+                $wrapper = $norms.find('.parcel'),
+                currType = $item.find('li').filter('.active').index(),
+                $specbox = $wrapper.find('.spec-box').eq(currType),
+                $pounditem = $specbox.find('.item-rule li').filter('.active');
+
+            b_productname = $norms.find('.hid_htmlname').val();
+            b_product_size = $pounditem.html().trim();
+            b_productprice_d = $pounditem.attr('data-prices');
+            b_productprice_m = parseFloat(b_productprice_d, 10).toFixed(2);
+            b_productCount_d = $specbox.find('.number_en').html();
+            b_productstyle = $item.find('li').filter('.active').html().trim();
+            // console.log(b_productname)
+            // console.log(b_product_size)
+            // console.log(b_productprice_d)
+            // console.log(b_productprice_m)
+            // console.log(b_productCount_d)
+            // console.log(b_productstyle)
+            // send to rxstream server
+			rxStream.track('add_shoppingcart', {
+				subject: {
+					o_username: o_username,
+					o_mobile: o_mobile
+				},
+				properties: {
+                      b_productname: b_productname,
+                      b_product_size: b_product_size,
+                      b_productprice_d: b_productprice_d,
+                      b_productprice_m: b_productprice_m,
+                      b_productCount_d: b_productCount_d,
+                      b_productstyle: b_productstyle,
+					  b_device: b_device
+				}
+			});
+
+            e.stopPropagation();
+
+        });
+
+        //喜欢
+        $('#main').on('click','.collect',function(e){
+            var b_productname = '',
+                b_linkornot = '';
+            var $item = $(this).closest('.pic');
+
+            b_productname = $item.find('.change').html();
+            b_linkornot = $(this).hasClass('selected') ? '取消喜欢' : '喜欢';
+            //console.log(b_productname)
+            //console.log(b_linkornot)
+            // send to rxstream server
+			rxStream.track('like', {
+    				subject: {
+    					o_username: o_username,
+    					o_mobile: o_mobile
+    				},
+    				properties: {
+                        b_productname: b_productname,
+                        b_linkornot: b_linkornot,
+    					b_device: b_device
+    				}
+    			});
+
+          e.preventDefault();
+          e.stopPropagation();
+        });
+
+        //定位城市
+        $('.city-two').find('li').on('click',function(){
+            var b_positioncity = '';
+
+            b_positioncity = $(this).text().trim();
+            //console.log(b_positioncity)
+            // send to rxstream server
+            rxStream.track('positioncity', {
+                subject: {
+                    o_username: o_username,
+                    o_mobile: o_mobile
+                },
+                properties: {
+                    b_positioncity: b_positioncity,
+                    b_device: b_device
+                }
+            });
+        });
+
+        //首页筛选
+        $('.list-two').find('li').click(function(){
+            var b_menu ='';
+            b_menu = $(this).text().trim();
+            //console.log(b_menu);
+            rxStream.track('positioncity', {
+                subject: {
+                    o_username: o_username,
+                    o_mobile: o_mobile
+                },
+                properties: {
+                    b_menu: b_menu,
+                    b_device: b_device
+                }
+            });
+        });
+    }
 })(jQuery, window, document);
--- src/assets/js/order-submit.js
+++ src/assets/js/order-submit.js
@@ -33,6 +33,9 @@
 		mother();
 
 		fnInitPayment();
+
+		// 瑞雪检测 --- 订单提交
+    	fnInitRxOrderSubmit();
 	});
 
 
@@ -180,7 +183,7 @@
 					cn: '芒果拿破仑',
 					en: 'Mango Napoleon'
 				},
-	            cost:'￥189',
+	            cost:'189',
 	            poundage:'2.5',
 	            attrs:['免费赠送5套餐具','可切分'],
 	            motherDay:true,
@@ -192,7 +195,7 @@
 					cn: '芒果拿破仑',
 					en: 'Mango Napoleon'
 				},
-				cost:'￥189',
+				cost:'189',
 				poundage:'2.5',
 				attrs:['免费赠送5套餐具','可切分'],
 				mother:true
@@ -427,9 +430,6 @@
 
 	})();
 
-
-
-
 //	生日牌选择
 	;(function(){
 		window.adaptive.desinWidth = 750;
@@ -463,8 +463,8 @@
 	        });
 	    });
 	})();
-//	支付方式选择
 
+//	支付方式选择
 	function fnInitPayment() {
 		var $oPaymentPopup = $('.payment-popup'),
 			$oPaymentAction = $('#paymentAction'),
@@ -529,6 +529,7 @@
 		});
 	}
 //	发票选择
+
 	;(function(){
 		window.adaptive.desinWidth = 750;
         window.adaptive.init();
@@ -568,6 +569,7 @@
 	    });
 
 	})();
+
 //	订单备注选择
 	;(function(){
 		window.adaptive.desinWidth = 750;
@@ -605,24 +607,24 @@
 
 //	时间选择
 	;(function() {
-	window.adaptive.desinWidth = 750;
-    window.adaptive.init();
+		window.adaptive.desinWidth = 750;
+	    window.adaptive.init();
 
-	var selectContactDom = $('#select_contact');
-	var showContactDom = $('#show_contact');
-	var contactProvinceCodeDom = $('#contact_province_code');
-	var contactCityCodeDom = $('#contact_city_code');
-	selectContactDom.bind('click', function() {
+		var selectContactDom = $('#select_contact');
+		var showContactDom = $('#show_contact');
+		var contactProvinceCodeDom = $('#contact_province_code');
+		var contactCityCodeDom = $('#contact_city_code');
+		selectContactDom.bind('click', function() {
 
-		var sccode = showContactDom.attr('data-city-code');
-		var scname = showContactDom.attr('data-city-name');
+			var sccode = showContactDom.attr('data-city-code');
+			var scname = showContactDom.attr('data-city-name');
 
-		var oneLevelId = showContactDom.attr('data-province-code');
-		var twoLevelId = showContactDom.attr('data-city-code');
-		var threeLevelId = showContactDom.attr('data-district-code');
-		var iosSelect = new IosSelect(3,
-			[year, month, day],
-			{
+			var oneLevelId = showContactDom.attr('data-province-code');
+			var twoLevelId = showContactDom.attr('data-city-code');
+			var threeLevelId = showContactDom.attr('data-district-code');
+			var iosSelect = new IosSelect(3,
+				[year, month, day],
+				{
 				title: '时间选择',
                 itemHeight: 0.933333,
                 headerHeight: 1.18,
@@ -645,6 +647,7 @@
 			});
 		});
 	})()
+
 //	生日贺卡选择
 	;(function(){
 		window.adaptive.desinWidth = 750;
@@ -732,4 +735,117 @@
 			});
 		})();
 	})();
+
+	// 瑞雪检测 --- 订单提交
+	function fnInitRxOrderSubmit(){
+		if(!rxStream){
+            return false;
+        }
+        var $hid_username = $('#hid_username');
+
+        var o_username = '',
+            o_mobile = '',
+            b_device = 'wap';
+
+        if($hid_username.length > 0){
+            o_username = $hid_username.val();
+            o_mobile = $hid_username.val();
+        }
+
+		//去结算
+        $('.closes').on('click','.closing',function(index, item){
+
+            var $cake_list = $('#cake_list'),
+				$sinfo = $('.order-information'),
+                $items = $cake_list.find('.item'),
+                shoppingcart = [],
+				submit_order = {},
+		        s_total = 0,
+		        s_money = 0,
+		        s_count = 0,
+		        s_coupon = 0,
+		        s_sales = 0,
+		        discount_price = 0,
+		        discount_type = '无';
+
+				//商品总金额
+				s_total = parseFloat($sinfo.find('.s-total').html().trim()).toFixed(2);
+				//去结算时的金额
+				s_money = parseFloat($(this).closest('.closes').find('.s-money').html()).toFixed(2);
+				//优惠券
+				s_coupon = parseFloat($sinfo.find('.s-coupon').html().trim()).toFixed(2);
+				//促销价
+				s_sales = parseFloat($sinfo.find('.s-sales').html().trim()).toFixed(2);
+
+				if(s_coupon != 0) {
+			        discount_price = s_coupon;
+			        discount_type = '优惠券';
+			    } else if(s_sales != 0) {
+			        discount_price = s_sales;
+			        discount_type = '促销';
+			    } else {
+			        discount_price = 0;
+			        discount_type = '无';
+			    }
+
+			$items.each(function(index, item) {
+                var b_productstyle = '',
+                    b_productname = '',
+                    b_product_size = '',
+                    b_productprice_d = '',
+                    b_productprice_m = 0,
+                    b_productCount_d = 0;
+
+                    b_productstyle = $(item).attr('data-style').trim();
+                    b_productname = $(item).attr('data-name').trim();
+                    b_product_size = $(item).attr('data-size').trim();
+                    b_productprice_d = $(item).attr('data-price').trim();
+                    b_productprice_m = parseFloat($(item).attr('data-price').trim()).toFixed(2);
+                    b_productCount_d = parseInt($(item).attr('data-amount').trim(), 10);
+
+                    shoppingcart.push({
+                      b_productstyle: b_productstyle,
+                      b_productname: b_productname,
+                      b_product_size: b_product_size,
+                      b_productprice_d: b_productprice_d,
+                      b_productprice_m: b_productprice_m,
+                      b_productCount_d: b_productCount_d,
+                      b_device: b_device
+                    });
+              });
+                // send submit_shoppingcart to rxstream server
+                 $.each(shoppingcart, function(index, detail) {
+                   rxStream.track('submit_shoppingcart', {
+                           subject: {
+                               o_username: o_username,
+                               o_mobile: o_mobile
+                           },
+                           properties: detail
+                       });
+                 });
+
+		   // b_orderprice 订单金额
+	       // b_order_amount 实际支付金额
+	       // b_order_count 商品件数
+	       // b_discountprice 优惠金额
+	       // b_discounttype 优惠类型
+	       submit_order.b_orderprice = s_total;
+	       submit_order.b_order_amount = s_money;
+	       submit_order.b_order_count = s_count;
+	       submit_order.b_discountprice = discount_price;
+	       submit_order.b_discounttype = discount_type;
+	       submit_order.b_device = b_device;
+		   console.log(submit_order.b_orderprice)
+	       // send submit_order to rxstream server
+	 			rxStream.track('submit_order', {
+	 				subject: {
+	 					o_username: o_username,
+	 					o_mobile: o_mobile
+	 				},
+	 				properties: submit_order
+	 			});
+
+        });
+	}
+
 })(jQuery, window, document);
