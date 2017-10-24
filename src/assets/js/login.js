3ff3ecdb23cadc6c87a440e549a2947ed77a4653
armdong <armdong@163.com>
2017-10-24 11:21:02 +0800

    快速登录页面滑动解锁功能完善

--- src/assets/js/login.js
+++ src/assets/js/login.js
@@ -1,27 +1,27 @@
 $(function() {
+    fnInitSlideLock();
+});
+
+// 滑动解锁
+function fnInitSlideLock() {
 
     var $oMaskAction = $('#maskAction'),
         $oBtn = $oMaskAction.find('.button'),
         $oSlider = $oMaskAction.find('.slider'),
         $oTrack = $oMaskAction.find('.track'),
-        $bg_green = $oMaskAction.find('.bg-green'),
-        iW, iLeft,
-        count = 0, // 验证重复获取验证码
-        time = 89,
+        $bgGreen = $oMaskAction.find('.bg-green'),
+        disX, iLeft,
+        time = 10,
         isMoving = false,
         isCompleted = false;
 
-
-
     $oBtn.on('touchstart.action', function(e) {
-        if (isCompleted) {
-                return false;
-            }
-
-        isMoving = true;
+        if (isCompleted || isMoving) {
+            return false;
+        }
 
         var touches = e.originalEvent.touches[0];
-        iW = touches.clientX - $oBtn.offset().left;
+        disX = touches.clientX - $oBtn.position().left;
         $oBtn.removeClass('button-on');
         $oTrack.removeClass('track-on');
 
@@ -29,13 +29,15 @@ $(function() {
     });
 
     $oBtn.on("touchmove.action", function(e) {
-        if (isCompleted || !isMoving) {
+
+        if (isCompleted) {
             return false;
         }
 
-        $oBtn.removeClass('active');
+        isMoving = true;
+
         var touches = e.originalEvent.touches[0];
-        iLeft = touches.clientX - iW;
+        iLeft = touches.clientX - disX;
         var disW = $oSlider.width() - $oBtn.width();
         if (iLeft <= 0) {
             iLeft = 0;
@@ -61,15 +63,15 @@ $(function() {
 
         var disW = $oSlider.width() - $oBtn.width();
         if (iLeft >= disW) {
-            $oBtn.css({
+            $oBtn.removeClass('active').css({
                 left: disW + 'px'
             });
             $oTrack.width(disW);
-
-            verification($bg_green);
             isCompleted = true;
+
+            verification();
         } else {
-            $oBtn.css({
+            $oBtn.addClass('active').css({
                 left: '0'
             });
             $oTrack.css({
@@ -84,20 +86,92 @@ $(function() {
     function defaultEvent(e) {
         e.preventDefault();
     }
-    function verification(obj) {
-        if (count == 0) {
-            count = 1;
-            obj.text('89" 后重新发送');
+    
+    // 滑动到最右边时处理函数
+    function verification() {
 
-            var interval = setInterval(function() {
-                obj.text(time-- + '" 后重新发送');
-                if (time == -1) {
-                    count = 0;
-                    clearInterval(interval);
-                    time = 89;
-                    obj.text('向右滑动获取动态验证码');
-                }
-            }, 1000);
+        var $mobile = $('.txt-mobile'),
+            txtMobile = $mobile.val().trim(),
+            mobileReg = /^(?=\d{11}$)^1(?:3\d|4[57]|5[^4\D]|7[^249\D]|8\d)\d{8}$/;
+
+        // 手机号合法性验证
+        if (txtMobile === '') {
+            $bgGreen.text('手机号码不能为空');
+            setTimeout(fnReset, 2000);
+            return false;
+        }
+
+        // 手机号合法性验证
+        if (!mobileReg.test(txtMobile)) {
+            $bgGreen.text('请输入正确的手机号码');
+            setTimeout(fnReset, 2000);
+            return false;
+        }
+
+        // TODO 处理发送短信验证码逻辑
+        handler4CountDown($bgGreen, 90, fnReset);
+        
+    }
+
+    function fnReset() {
+        $oBtn.addClass('active').css({
+            left: '0'
+        });
+        $oTrack.css({
+            width: '0'
+        });
+        $bgGreen.empty();
+        isCompleted = false;
+    }
+}
+
+/**
+ * 倒计时处理程序
+ * @param  {[type]} $oTarget        [目标元素]
+ * @param  {[type]} timeToCountDown [倒计时秒数]
+ * @param  {[type]} callback        [倒计时结束后回调函数]
+ * @return {[type]}                 [description]
+ */
+function handler4CountDown($oTarget, timeToCountDown, callback) {
+    var timer = null;
+    var beginTime = new Date().getTime();
+    var endTime = beginTime + timeToCountDown * 1000;
+    var curShowTimeSeconds = 0;
+
+    curShowTimeSeconds = getCurrentShowTimeSeconds();
+
+    timer = setInterval(function() {
+        render();
+        update();
+    }, 50);
+
+    function update() {
+        var nextShowTimeSeconds = getCurrentShowTimeSeconds();
+        var nextSeconds = nextShowTimeSeconds;
+        var curSeconds = curShowTimeSeconds;
+        if (nextSeconds !== curSeconds) {
+            curShowTimeSeconds = nextShowTimeSeconds;
         }
     }
-});
+
+    function render() {
+        var seconds = curShowTimeSeconds;
+        if (seconds === 0) {
+            clearInterval(timer);
+
+            // 倒计时完成把isSend设成空字符串
+            $oTarget.text('');
+
+            callback && callback();
+        } else {
+            $oTarget.html('短信验证码已发送，' + seconds + 's 后重新获取');
+        }            
+    }
+
+    function getCurrentShowTimeSeconds() {
+        var curTime = new Date().getTime();
+        var ret = endTime - curTime;
+        ret = Math.round(ret / 1000);
+        return ret >= 0 ? ret : 0;
+    }
+}
